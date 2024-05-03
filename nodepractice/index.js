37b1f6117244ea0933b8d834dbab40061664280f
FullStackJinnnn <ddoljin@gmail.com>
2024-05-03 21:26:58 +0900

    node.js login study

--- nodepractice/index.js
+++ nodepractice/index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser');
+
+const config = required('./config/key');
+
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded 를 분석해서 가져옴
@@ -10,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://ddoljin:1234@cluster0.6vtyedu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => console.log('MongoDB Connected!')).catch(err => console.log(err))
+mongoose.connect(config.mongoURI).then(() => console.log('MongoDB Connected!')).catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
@@ -37,6 +40,32 @@ app.get('/', (req, res) => {
       res.json({ success: false, err })
     })
   })
+
+  app.post('/login', async (req,res) => {
+    //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+    User.findOne({ email: req.body.email }), (err, userInfo) => {
+      if(!user) {
+        return res.json({
+          loginSuccess: false,
+          message: "유저 없음"
+        })
+      }
+    }
+  })
+  //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+
+      user.comparePassword(req.body.password , (err, isMatch ) => {
+        if(!isMatch)
+        return res.json({ loginSuccess: false, message: "비밀번호 틀림"})
+    //비밀번호 까지 맞다면 토큰을 생성하기.
+        user.generateToken((err, user) => {
+
+      })
+
+    })
+
+
+  
 
 
 
