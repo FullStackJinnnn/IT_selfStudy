2508495030d89c962d6a179f982f862216a94ddc
FullStackJinnnn <ddoljin@gmail.com>
2024-05-17 23:04:02 +0900

    baekjoon solved

--- codetest/src/easy/code/test/Main.java
+++ codetest/src/easy/code/test/Main.java
@@ -1,70 +1,40 @@
 package easy.code.test;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
+		Queue<Integer> queue = new LinkedList<>();
 
-		Scanner scan = new Scanner(System.in);
-
-// 사용자 입력
-
-		int input = scan.nextInt();
-
-		Main main = new Main();
-
-		main.solution(input);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String input = br.readLine();
+		String numArr[] = input.split(" ");
+		int interval = 1;
+		for (int i = 0; i < Integer.parseInt(numArr[0]); i++) {
+			queue.add(i+1);
+		}
+		System.out.print("<");
+		while (!queue.isEmpty()) {
+			int var = queue.poll();
+			if (interval % Integer.parseInt(numArr[1]) != 0) {
+				queue.add(var);
+				
+			}	else if (queue.size()==0) {
+					System.out.print(var);
+				}
+			else {
+				System.out.print(var+", ");
+			}
+			interval++;
+		}
+		System.out.print(">");
 
 	}
-
-	public int solution(int input) {
-
-		// 소수를 모두 합한 변수 초기화
-
-		// input은 최소 4부터 주어지기에 소수 2,3을 더한값으로 초기화
-
-		int primeNumTotal = 5;
-
-		// 4부터 input값까지 순회하며 반복
-
-		for (int i = 4; i <= input; i++) {
-
-		// 소수인지 확인을 위한 변수 초기회
-
-		int divideCnt = 0;
-
-		// 1과 자기자신은 무조건 나눴을때 나머지가 0이기에 제외
-
-		for (int j = 2; j < i; j++) {
-
-		// 나눴을때 나머지가 0이되는 숫자가 있는지 확인
-
-		if (i % j == 0) {
-
-		divideCnt++;
-
-		}
-
-		}
-
-		// 나눠지는 숫자가 없었다면 i는 소수이기에 총합에 더하기
-
-		if (divideCnt == 0) {
-
-		primeNumTotal += i;
-
-		}
-
-		}
-
-		System.out.print("> " + primeNumTotal);
-
-		// 모든 소수의 합 리턴
-
-		return primeNumTotal;
-
-		}
-
 }
