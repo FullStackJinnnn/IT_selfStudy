5f882e27adb4025208cc2691e3409926b61363cd
FullStackJinnnn <ddoljin@gmail.com>
2024-06-03 23:00:15 +0900

    login with react

--- codetest/src/easy/code/test/Main.java
+++ codetest/src/easy/code/test/Main.java
@@ -7,34 +7,34 @@ import java.util.Arrays;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-
-		int T = Integer.parseInt(br.readLine());
-
-	
-		int[][] arr = new int[15][15];
-		long n1 = 0;
-		long n2 = 0;
-		long fiboFloor = 0;
-		int total = 0;
-		for (int i = 0; i < 15; i++) {
-			arr[0][i] = i + 1;
-		}
-		for (int i = 1; i < 15; i++) {
-			for (int j = 0; j < 15; j++) {
-				if (j == 0) {
-					arr[i][j] = 1;
-				} else {
-					arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
-				}
-			}
-		}
-		for (int i = 0; i < T; i++) {
-			int n = Integer.parseInt(br.readLine());
-			int k = Integer.parseInt(br.readLine());
-
-				System.out.println(arr[n][k-1]);
-			}
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		StringBuilder sb = new StringBuilder();
+//
+//		int T = Integer.parseInt(br.readLine());
+//
+//	
+//		int[][] arr = new int[15][15];
+//		long n1 = 0;
+//		long n2 = 0;
+//		long fiboFloor = 0;
+//		int total = 0;
+//		for (int i = 0; i < 15; i++) {
+//			arr[0][i] = i + 1;
+//		}
+//		for (int i = 1; i < 15; i++) {
+//			for (int j = 0; j < 15; j++) {
+//				if (j == 0) {
+//					arr[i][j] = 1;
+//				} else {
+//					arr[i][j] = arr[i][j - 1] + arr[i - 1][j];
+//				}
+//			}
+//		}
+//		for (int i = 0; i < T; i++) {
+//			int n = Integer.parseInt(br.readLine());
+//			int k = Integer.parseInt(br.readLine());
+//
+//				System.out.println(arr[n][k-1]);
+//			}
 		}
 	}
